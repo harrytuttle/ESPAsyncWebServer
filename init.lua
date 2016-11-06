844920a06340c376a5d2129112d08aa1222fd206
harrytuttle <archibald.tuttle@gmail.com>
2016-11-06 20:55:58 +0100

    Fix arg mismatch in invocation

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ tmr.alarm(0, 1000, 1, function()
   if wifi.sta.getip() then
     tmr.stop(0)
     if mdns then mdns.register(wifi.sta.gethostname(),{service="http",port=80})end print(wifi.sta.getip(),wifi.sta.gethostname())
-    bcast=require("httpd")(nil,nil,function(msg)return msg and uart.write(0,msg)or"connected"end)
+    bcast=require("httpd")(nil,nil,nil,function(msg)return msg and uart.write(0,msg)or"connected"end)
     --tmr.alarm(5,2500,1,function()bcast(""..node.heap())end)
     uart.on("data",1,bcast,1)
   end
