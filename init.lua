16c852f59951a2dfc82b4575ddf4ee2a9cbfd0b8
harrytuttle <archibald.tuttle@gmail.com>
2016-11-13 04:15:46 +0100

    By convention assume that the hostname is equal to the ap ssid
    This is convenient because ap ssid and pwd are stored in the last 20k
    of the flash memory, past the end of the spiffs filesystem, so they
    survive not only a reboot but also a nodemcu upgrade and and even
    a filesystem format.

--- init.lua
+++ init.lua
@@ -1,11 +1,12 @@
 --wifi.setmode(wifi.STATION)wifi.sta.config("SSID","KEY")wifi.sta.autoconnect(1)
-wifi.sta.sethostname("nodemcu")
+wifi.sta.sethostname(wifi.ap.getconfig())
 print("Trying ", wifi.sta.getconfig())
 print("waiting 1 second or tmr.stop(0)")
 tmr.alarm(0, 1000, 1, function()
   if wifi.sta.getip() then
     tmr.stop(0)
-    if mdns then mdns.register(wifi.sta.gethostname(),{service="http",port=80})end print(wifi.sta.getip(),wifi.sta.gethostname())
+    if mdns then mdns.register(wifi.sta.gethostname(),{service="http",port=80})end
+    print(wifi.sta.getip(),wifi.sta.gethostname())
     bcast=require("httpd")(nil,nil,nil,function(msg)return msg and uart.write(0,msg)or"connected"end)
     --tmr.alarm(5,2500,1,function()bcast(""..node.heap())end)
     uart.on("data",1,bcast,1)
